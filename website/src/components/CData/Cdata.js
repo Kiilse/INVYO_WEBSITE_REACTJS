3bcd86b7edc222759056e1fee9834c4c5d0319d1
Cuvelier Victor <victor.cuvelier@epitech.eu>
2021-02-23 00:49:22 +0100

    [+] add data tab

--- website/src/components/CData/Cdata.js
+++ website/src/components/CData/Cdata.js
@@ -1,37 +1,50 @@
 import * as React from 'react';
 import { DataGrid } from '@material-ui/data-grid';
-import "./Cdata.css"
+import FormControl from 'react-bootstrap/FormControl';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
 import art from '../../data'
 
 const columns = [
-  { field: 'Title', headerName: 'Titre', width: 100 },
-  { field: 'Content', headerName: 'Contenu', width: 200 },
-  { field: 'Language', headerName: 'LANGUE', width: 130 },
-  { field: 'Tags', headerName: 'TAGS', width: 90 },
+  { field: 'Title', headerName: 'Title', width: 600 , height: 300},
+  { field: 'Content', headerName: 'Content', width: 800, height: 300 },
+  { field: 'Language', headerName: 'Language', width: 200, height: 300 },
+  { field: 'Tags', headerName: 'Tags', width: 200, height: 300},
 ];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+
 
 function createRow() {
     var row = [] 
-
+    var list = []
+    var i = 0
+    while (i < art.articles.length) {
+        list = {
+            id: i + 1,
+            Title: art.articles[i].Title,
+            Content: art.articles[i].Content,
+            Language: art.articles[i].Language,
+            Tags: art.articles[i].Tags
+        }
+        row.push(list)
+        i = i + 1
+    }
     return row
 }
 
+const rows = createRow()
+
 export default function DataTable() {
   return (
-    <div className="table">
-      <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
+    <div style={{ height: 750, width: '100%' }}>
+        <InputGroup className="mb-3">
+            <FormControl type="text" placeholder="Search"/>
+            <InputGroup.Append>
+                <Button variant="dark">Search</Button>
+            </InputGroup.Append>
+        </InputGroup>
+        <br/>
+        <DataGrid rows={rows} columns={columns} pageSize={15}/>
     </div>
   );
 }
